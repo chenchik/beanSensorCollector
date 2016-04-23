46e09b653c87ec31890bf2ca0f1c2ee0008466f6
Danila Chenchik <chenchik@email.unc.edu>
2016-04-23 13:33:53 -0400

    updating UI

--- app/src/main/java/comp590proj4/chenchik/comp590proj4/MainActivity.java
+++ app/src/main/java/comp590proj4/chenchik/comp590proj4/MainActivity.java
@@ -1,11 +1,19 @@
 package comp590proj4.chenchik.comp590proj4;
 
+import comp590proj4.chenchik.comp590proj4.R;
+
+import android.content.Context;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+//import android.R;
 
 import com.punchthrough.bean.sdk.Bean;
 import com.punchthrough.bean.sdk.BeanDiscoveryListener;
@@ -26,9 +34,11 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -39,6 +49,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     boolean keepLooping = false;
     private ArrayList<double[]> vals = new ArrayList<double[]>();
     private ArrayList<Integer> dates = new ArrayList<Integer>();
+    private int activitySerialNum = 1;
+    private int positionSerialNum = 1;
+    private int serialNum = 1;
+    //Button btn = (Button) findViewById(R.id.standing);
+    //RadioButton stand = (RadioButton) findViewById(R.id.standingButton);
+
+    private RadioButton[] activities;
+
+    private RadioButton[] positions;
+    String fileName = "";
+    boolean lobstarConnected = false;
+
+    //HashMap<String, Integer> fileMap = new HashMap<>();
+
+    int[][] fileNamearr = new int[3][6];
+    //to find positions in array
+    int STANDING = 0;
+    int SITTING = 1;
+    int WALKING = 2;
+    int RUNNING = 3;
+    int UPSTAIRS = 4;
+    int DOWNSTAIRS = 5;
+
+    int WAIST = 0;
+    int WRIST = 1;
+    int SHOW = 2;
+
+    String currentActivity = "";
+
+    /*String[] fileNames = {
+        "STANDING_WAIST_0",
+            "SITTING_WAIST_0",
+            "WALKING_WAIST_0",
+            "RUNNING_WAIST_0",
+            "UPSTAIRS_WAIST_0",
+            "DOWNSTAIRS_WAIST_0",
+            "STANDING_WRIST_0",
+            ""
+
+    };*/
 
 
     @Override
@@ -47,6 +97,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         BeanManager.getInstance().startDiscovery(bdl);
+        activities = new RadioButton[]{
+                (RadioButton) findViewById(R.id.standingButton),
+                (RadioButton) findViewById(R.id.sittingButton),
+                (RadioButton) findViewById(R.id.walkingButton),
+                (RadioButton) findViewById(R.id.runningButton),
+                (RadioButton) findViewById(R.id.upstairsButton),
+                (RadioButton) findViewById(R.id.downstairsButton)
+        };
+
+        positions = new RadioButton[]{
+                (RadioButton) findViewById(R.id.waistButton),
+                (RadioButton) findViewById(R.id.wristButton),
+                (RadioButton) findViewById(R.id.shoeButton)
+        };
+
+
+        //updateFileName();
     }
 
     BeanDiscoveryListener bdl  = new BeanDiscoveryListener() {
@@ -54,6 +121,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void onBeanDiscovered(Bean bean, int rssi) {
             Log.v("DANILA", "Bean Discovered, its name: " + bean.getDevice().getName() + " its rssi is: " + rssi );
             //b = bean;
+            showPop(bean.getDevice().getName()+" Discovered" + " its rssi is: " + rssi);
             beans.add(bean);
         }
 
@@ -62,6 +130,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             for(Bean bean : beans){
                 if(bean.getDevice().getName().equals("LobstarBean")){
                     b = bean;
+
                     break;
                 }
             }
@@ -81,7 +150,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 b.readAcceleration(new Callback<Acceleration>() {
                     @Override
                     public void onResult(Acceleration result) {
-                        Log.v("Result", "acceleration is: x " + result.x() +" y "+ result.y() +" z "+ result.z());
+                        Log.v("Result", "acceleration is: x " + result.x() + " y " + result.y() + " z " + result.z());
+                        showPop("LobstarBean Connected");
+                        TextView t = (TextView) findViewById(R.id.isConnected);
+                        t.setText("lobstarbean connected");
+                        lobstarConnected = true;
+
 
                     }
                 });
@@ -91,11 +165,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         public void onConnectionFailed() {
             Log.v("CONNECT FAILED", "---");
+            showPop("connection failed");
+            TextView t = (TextView) findViewById(R.id.isConnected);
+            t.setText("connection failed");
         }
 
         @Override
         public void onDisconnected() {
             Log.v("CONNECT DISCONNECTED", "---");
+            showPop("lobstar disconnected");
+            TextView t = (TextView) findViewById(R.id.isConnected);
+            t.setText("disconnected");
         }
 
         @Override
@@ -111,27 +191,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         public void onError(BeanError error) {
             Log.v("SOME KIND OF ERROR", "");
+            showPop("some kind error");
+            TextView t = (TextView) findViewById(R.id.isConnected);
+            t.setText("Some kind of error");
         }
 
     };
     public void startLoop(View v){
-        keepLooping = true;
-        if(b.isConnected()) {
-            Thread t = new Thread() {
-                public void run() {
-                    while (keepLooping) {
-                        try {
-                            startAccelReading(b);
-                            Thread.sleep(100);
-                        } catch (Exception e) {
-                            e.printStackTrace();
+        if(lobstarConnected) {
+            keepLooping = true;
+            if (b.isConnected()) {
+                Thread t = new Thread() {
+                    public void run() {
+                        while (keepLooping) {
+                            try {
+                                startAccelReading(b);
+                                Thread.sleep(100);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
                         }
 
                     }
-
-                }
-            };
-            t.start();
+                };
+                t.start();
+            }
+        }
+        else{
+            showPop("you are not connected to lobstarBean yet");
         }
 
 
@@ -145,7 +233,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void startAccelReading(Bean bean){
-        if (accel == null) {
+        /*if (accel == null) {
             bean.readAcceleration(new Callback<Acceleration>() {
                 @Override
                 public void onResult(Acceleration result) {
@@ -160,7 +248,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 }
             });
-        }
+        }*/
 
         bean.readAcceleration(new Callback<Acceleration>() {
             @Override
@@ -168,10 +256,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.v("Result", "acceleration is: x " + result.x() + " y " + result.y() + " z " + result.z());
                 double[] d = {result.x(), result.y(), result.z()};
                 vals.add(d);
-                Log.v("result arr", vals.get(0)[0]+"");
+                Log.v("result arr", vals.get(0)[0] + "");
                 Date dNow = new Date( );
                 SimpleDateFormat ft = new SimpleDateFormat("ssSSS");
                 dates.add(Integer.parseInt(ft.format(dNow)));
+                TextView seconds = (TextView) findViewById(R.id.timerText);
+                String sec = new SimpleDateFormat("ss").toString();
+                String ms = new SimpleDateFormat("SSS").toString();
+                seconds.setText(dNow.toString());
             }
         });
     }
@@ -195,9 +287,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         bw.close();
     }
+
+    public void checkActivity(View v){
+        RadioButton rb = (RadioButton) findViewById(v.getId());
+        for(int i = 0; i < activities.length; i++){
+            if(rb.getId() != activities[i].getId()){
+                activities[i].setChecked(false);
+            }
+            else{
+                activities[i].setChecked(true);
+                activitySerialNum = i;
+                //updateSerialNum(i + 1, positionSerialNum);
+                //updateFileName();
+            }
+        }
+
+    }
+    public void checkPosition(View v){
+        RadioButton rb = (RadioButton) findViewById(v.getId());
+        for(int i = 0; i < positions.length; i++){
+            if(rb.getId() != positions[i].getId()){
+                positions[i].setChecked(false);
+            }
+            else{
+                positions[i].setChecked(true);
+                positionSerialNum = i;
+                //updateSerialNum(activitySerialNum, i + 1);
+                //updateFileName();
+            }
+        }
+    }
+    public void updateSerialNum(int act, int pos){
+        serialNum = act*pos;
+    }
+
+    public void updateFileName(){
+        //TextView t = (TextView) findViewById(R.id.fileName);
+        String activity = activities[activitySerialNum].getText().toString();
+        String position = positions[positionSerialNum].getText().toString();
+        fileNamearr[positionSerialNum][activitySerialNum]++;
+        int fileNum = fileNamearr[positionSerialNum][activitySerialNum];
+        fileName = activity+ "_" + position+ "_" + fileNum + ".txt";
+        currentActivity = activity;
+
+        //t.setText("/"+currentActivity+"/"+fileName);
+
+
+    }
+
+    //public String getFileName(){
+
+    //}
     //function: copies the bytes received from the server int the retr_files folder
     public void createFile(View v) {
-        String youFilePath = Environment.getExternalStorageDirectory().toString()+"/outputwrite1.txt";
+        updateFileName();
+        //String youFilePath = Environment.getExternalStorageDirectory().toString()+"/"+fileName;
+
+        String youFilePath = Environment.getExternalStorageDirectory().toString()+"/"+currentActivity+"/"+fileName;
+        TextView tv = (TextView) findViewById(R.id.lastFileMadeText);
+        tv.setText(fileName);
         Log.v("FILEPATH", youFilePath);
         Log.v("first input", vals.get(0)[0]+"");
         Log.v("first date", dates.get(0)+"");
@@ -225,6 +373,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     //out.write((int)vals.get(i));
                 }
 
+                dates.clear();
+                vals.clear();
+
                 if (outWriter != null) {
                     outWriter.close();
 
@@ -235,5 +386,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     }
+
+    public void showPop(String s){
+        //context is a the window object of the application, we want to put something on top of the current window object
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        //toast is an alert/popup
+        Toast toast = Toast.makeText(context, s, duration);
+        toast.show();
+    }
+
 
 }
